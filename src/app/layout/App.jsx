c16d4007c77a3497e3b788f0e7ad532e7b97e179
Fawad Shah <fawadshah.thall@gmail.com>
2019-02-25 03:21:13 +0500

    Application for Deployment

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Re-Vents</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
